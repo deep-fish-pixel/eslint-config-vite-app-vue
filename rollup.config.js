493fa37c4bc41625fe5194f7290fe04459333af0
wei.ma1 <wei.ma1@nio.com>
2023-10-27 21:41:37 +0800

    fix: set commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,9 @@ export default {
     copy({
       targets: [
         { src: 'README.md', dest: 'dist/' },
-        { src: 'package.json', dest: 'dist/' }
+        { src: 'package.json', dest: 'dist/', transform(contents, name){
+            return Buffer.from(contents.toString().replace('"module"', '"commonjs"'))
+        } }
       ]
     })
   ]
